308b727a4dc00473520dd4b7a3674e83599b3c40
publicmm9 <87865796+publicmm9@users.noreply.github.com>
2021-09-22 22:18:34 +0630

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
-class Config:
-    AAA = "--help"
-    BBB = "what"
-    CCC = "fuck"
+import os
+import json
+from distutils.util import strtobool as stb
+
+# --------------------------------------
+first = ""
+second = ""
+third = ""
+
+# Example: OWNER_ID = 619418070
+
+# dont edit below this >
+first = os.environ.get('first', first)
+second = os.environ.get('second', second)
+third = os.environ.get('third', third)
